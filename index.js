6075bd75ae524cf0117601d088e5f5c06fee0ba5
pratapbodkhe1 <pratap.bodkhe1@gmail.com>
2026-07-23 10:17:08 +0530

    Added Gemini AI

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const { GoogleGenAI } = require("@google/genai");
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 app.get("/", (req, res) => {
-    res.send("🚀 Business Guidance AI Server Running!");
+  res.send("🚀 Business Guidance AI is Running!");
+});
+
+app.post("/ask", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: message,
+    });
+
+    res.json({
+      reply: response.text,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "AI Error",
+    });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
